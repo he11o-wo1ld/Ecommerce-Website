dc5927f479c851492cfe07dfdd5ff583e376522e
krishdp <95845806+krishdp@users.noreply.github.com>
2022-05-21 01:02:17 +0530

    Added Add to cart Option.

--- ecomsite/ecomsite/urls.py
+++ ecomsite/ecomsite/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', indexView, name='index'),
     path('<int:id>/', detailView, name='detail'),
+    path('checkout/', checkout, name='checkout'),
 ]
--- ecomsite/shop/models.py
+++ ecomsite/shop/models.py
@@ -11,3 +11,16 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+class Order(models.Model):
+    items = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=70)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    zipcode = models.CharField(max_length=255)
+    total = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
